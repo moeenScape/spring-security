29968f2199a6458224917aa5a91cbfcc8133f2cd
moeen775 <moeenahmmed2015br@gmail.com>
2024-01-01 01:21:49 +0600

    Happy New Year

--- src/main/java/com/moeen/springsecurity/config/ApplicationAuditAware.java
+++ src/main/java/com/moeen/springsecurity/config/ApplicationAuditAware.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
 public class ApplicationAuditAware implements AuditorAware<Integer> {
     @Override
     public Optional<Integer> getCurrentAuditor() {

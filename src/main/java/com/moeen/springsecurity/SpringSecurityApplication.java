ea74b840ef38ade01baab38dba4c5eac0c78ae39
moeen775 <moeenahmmed2015br@gmail.com>
2024-01-04 23:35:02 +0600

    code Modification

--- src/main/java/com/moeen/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/moeen/springsecurity/SpringSecurityApplication.java
@@ -1,15 +1,8 @@
 package com.moeen.springsecurity;
 
-import com.moeen.springsecurity.entity.model.RegisterRequest;
-import com.moeen.springsecurity.service.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import static com.moeen.springsecurity.entity.Role.ADMIN;
-import static com.moeen.springsecurity.entity.Role.MANAGER;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")

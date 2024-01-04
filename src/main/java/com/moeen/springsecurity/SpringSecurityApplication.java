978bf1ebfc8e7aee0f4f23b5d8294974d380fbe0
moeen775 <moeenahmmed2015br@gmail.com>
2024-01-04 23:34:17 +0600

    Modification of Auditing.

--- src/main/java/com/moeen/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/moeen/springsecurity/SpringSecurityApplication.java
@@ -18,31 +18,4 @@ public class SpringSecurityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(
-			UserService service
-	) {
-		return args -> {
-			var admin = RegisterRequest.builder()
-					.firstname("Admin")
-					.lastname("Admin")
-					.email("admin@mail.com")
-					.password("password")
-					.role(ADMIN)
-					.build();
-			System.out.println("Admin token: " + service.register(admin).getAccessToken());
-
-			var manager = RegisterRequest.builder()
-					.firstname("Admin")
-					.lastname("Admin")
-					.email("manager@mail.com")
-					.password("password")
-					.role(MANAGER)
-					.build();
-			System.out.println("Manager token: " + service.register(manager).getAccessToken());
-
-		};
-	}
-
 }
